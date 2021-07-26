8deafed882524797ab9c3600f596ebe4bb613be4
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-26 20:47:18 +0300

    adding a new new template

--- wifi/settings.py
+++ wifi/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
+    'network',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,10 @@ WSGI_APPLICATION = 'wifi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wifi',
+        'USER':'moringa',
+        'PASSWORD': 'Access',
     }
 }
 
